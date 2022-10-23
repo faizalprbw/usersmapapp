2f793e09b5cca66767cf5363d25df2ee11f4b7d5
Akhmad Faizal Prabowo Sarwono <faizalprbw@gmail.com>
2022-10-23 22:59:54 +0700

    add pytest & github action CI

--- usersmapapp_api/urls.py
+++ usersmapapp_api/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.urls import reverse_lazy
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from rest_framework.authtoken.views import obtain_auth_token
+from userprofile.views import dashboard_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', dashboard_view),
+    path('admin/', admin.site.urls),
     path('api/v1/', include('userprofile.urls')),
     path('api/v1/auth/', obtain_auth_token)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
